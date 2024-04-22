30c190fa79aabd0aa00cddd119a7b1ac3e3bf080
Diahayuups <fhoodhyps@gmail.com>
2024-04-22 21:20:10 +0700

    Praktikum Web ke 7

--- project01/pasien/sidebar.php
+++ project01/pasien/sidebar.php
@@ -1,8 +1,14 @@
 <!-- Main Sidebar Container -->
+<?php
+session_start();
+if (!$_SESSION['user']) {
+    header("location:login.php");
+}
+?>
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
@@ -12,10 +18,10 @@
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="../dist/img/avatar4.png" class="img-circle elevation-2" alt="User Image">
+                <img src="dist/img/avatar4.png" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Diah ayu puspasari</a>
+                <a href="#" class="d-block"><?= $_SESSION['user']['username']; ?></a>
             </div>
         </div>
 
@@ -46,19 +52,19 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="index.php" class="nav-link">
+                            <a href="pasien" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Table Pasien</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="../dokter" class="nav-link">
+                            <a href="dokter" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Table Dokter</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="../periksa" class="nav-link">
+                            <a href="periksa" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Table Periksa</p>
                             </a>
